e1127c4bc5627896d266d1c6e9d976411c5f0474
Segni Tulu <Segni38467@marvin.com>
2023-09-26 14:31:59 -0500

    simple fix in project for fixing the type

--- components/project.tsx
+++ components/project.tsx
@@ -1,14 +1,11 @@
+"use client";
+
 import { useRef } from "react";
+import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-type ProjectProps = {
-  title: string;
-  description: string;
-  tags: string[];
-  imageUrl: string;
-  linkUrl: string; 
-};
+type ProjectProps = (typeof projectsData)[number];
 
 export default function Project({
   title,
@@ -22,24 +19,24 @@ export default function Project({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
-  // Handle the click event 
-  const handleCardClick = () => {
-    window.location.href = linkUrl;
-  };
+    // Handle the click event to navigate to the link URL
+    const handleCardClick = () => {
+      window.location.href = linkUrl;
+    };
+
 
   return (
     <motion.div
       ref={ref}
       style={{
-        scale: scaleProgress,
-        opacity: opacityProgress,
+        scale: scaleProgess,
+        opacity: opacityProgess,
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      {/* Wrap the project card with a clickable anchor element */}
       <a href={linkUrl} target="_blank" rel="noopener noreferrer" onClick={handleCardClick} className="group">
         <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
           <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
